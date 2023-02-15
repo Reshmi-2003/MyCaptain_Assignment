92ae60d94625983e509864f654b711816d72cc78
Reshmi-2003 <112300517+Reshmi-2003@users.noreply.github.com>
2023-02-15 20:13:01 +0530

    Add files via upload

---
+++ postive num.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 20:00:44 2023
+
+@author: Elite
+"""
+
+
+start, end = -4, 19
+
+for num in range(start, end + 1):
+
+	if num >= 0:
+		print(num, end=" ")
